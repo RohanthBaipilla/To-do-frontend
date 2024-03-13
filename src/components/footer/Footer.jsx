c97af67601468f2680c0f33bca015513af7779fc
BAIPILLA SWAMY ESHWAR ROHANTH <107876880+RohanthBaipilla@users.noreply.github.com>
2024-03-13 11:17:15 +0530

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="container-fluid p-3 d-flex justify-content-center align-items-center  footer">
+      <p>TO-DO APP</p> &nbsp; <p>&copy;rohanthbaipilla</p>
+    </div>
+  );
+};
+
+export default Footer;
